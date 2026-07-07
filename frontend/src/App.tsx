00119d0f0ae3f41fef88869668978f4fb3cd3e20
Benel <you@example.com>
2026-07-07 21:40:36 +0100

    refactor: remove Products & Trucks routes, reset DailyArrivals to empty page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,13 +5,11 @@ import { Layout } from './components/Layout';
 import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
 import ClientsPage from './pages/ClientsPage';
-import ProductsPage from './pages/ProductsPage';
 import InvoicesPage from './pages/InvoicesPage';
 import PaymentsPage from './pages/PaymentsPage';
 import CaissePage from './pages/CaissePage';
 import UsersPage from './pages/UsersPage';
 import DailyArrivals from './pages/DailyArrivals';
-import TrucksPage from './pages/TrucksPage';
 import StockPage from './pages/StockPage';
 import AlertsPage from './pages/AlertsPage';
 import WholesaleInvoiceTracker from './pages/WholesaleInvoiceTracker';
@@ -27,14 +25,12 @@ export default function App() {
             <Route element={<Layout />}>
               <Route index element={<DashboardPage />} />
               <Route path="clients" element={<ClientsPage />} />
-              <Route path="products" element={<ProductsPage />} />
               <Route path="invoices" element={<InvoicesPage />} />
               <Route path="payments" element={<PaymentsPage />} />
               <Route path="caisse" element={<CaissePage />} />
               <Route path="alerts" element={<AlertsPage />} />
               <Route path="users" element={<ProtectedRoute roles={['boss', 'manager']}><UsersPage /></ProtectedRoute>} />
-              <Route path="arrivals" element={<ProtectedRoute roles={['boss', 'manager', 'warehouse']}><DailyArrivals /></ProtectedRoute>} />
-              <Route path="trucks" element={<ProtectedRoute roles={['boss', 'manager']}><TrucksPage /></ProtectedRoute>} />
+              <Route path="arrivals" element={<DailyArrivals />} />
               <Route path="stock" element={<ProtectedRoute roles={['boss', 'manager', 'warehouse']}><StockPage /></ProtectedRoute>} />
               <Route path="invoice-tracker" element={<WholesaleInvoiceTracker />} />
               <Route path="crate-tracker" element={<CrateMovementTracker />} />
